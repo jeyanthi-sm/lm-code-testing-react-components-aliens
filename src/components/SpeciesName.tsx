53a5af4b3a6bd3c32617c13d690af60744346198
jeyanthi-sm <111299753+jeyanthi-sm@users.noreply.github.com>
2023-02-03 23:00:25 +0000

    First commit after adding SpecieName Component

---
+++ src/components/SpeciesName.tsx
@@ -0,0 +1,24 @@
+import React, { EventHandler } from "react";
+import { text } from "stream/consumers";
+import { useState } from "react";
+
+export interface SpeciesNameProps {
+  speciesName: string;
+  onChangeHandler: (ev: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function SpeciesName(inputSpeciesName: SpeciesNameProps) {
+  return (
+    <>
+      <label htmlFor="speciesName"> Species Name </label>
+      <input
+        id="speciesName"
+        type="text"
+        value={inputSpeciesName.speciesName}
+        onChange={inputSpeciesName.onChangeHandler}
+      />
+    </>
+  );
+}
+
+export default SpeciesName;
--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,13 +1,27 @@
-import { useState } from 'react';
-import W12MHeader from './W12MHeader';
+import { useState } from "react";
+import SpeciesName from "./SpeciesName";
+import W12MHeader from "./W12MHeader";
 
 const W12MForm = () => {
-	return (
-		<section className='w12MForm'>
-			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
-		</section>
-	);
+  const [speciesName, setSpeciesName] = useState<string>("humans");
+
+  const changeEventHandler = (ev: React.ChangeEvent<HTMLInputElement>): void =>
+    setSpeciesName(ev.target.value);
+
+  const [textAreaValue, setTextAreaValue] = useState<string>("");
+  const [selectValue, setSelectValue] = useState<string>("optionA");
+
+  return (
+    <section className="w12MForm">
+      <W12MHeader />
+      {/* REST OF FORM GOES HERE */}
+
+      <SpeciesName
+        speciesName={speciesName}
+        onChangeHandler={changeEventHandler}
+      />
+    </section>
+  );
 };
 
 export default W12MForm;
