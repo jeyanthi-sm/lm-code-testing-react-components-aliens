5addaacda7b637f5732479041bc99326f0eadca8
jeyanthi-sm <111299753+jeyanthi-sm@users.noreply.github.com>
2023-02-03 23:02:41 +0000

    Added TextArea and Select tag

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -20,6 +20,26 @@ const W12MForm = () => {
         speciesName={speciesName}
         onChangeHandler={changeEventHandler}
       />
+
+      <label htmlFor="ReasonForSparing"> Reason For Sparing </label>
+      <textarea //Reason for sparing
+        id="ReasonForSparing"
+        value={textAreaValue}
+        onChange={(ev: React.ChangeEvent<HTMLTextAreaElement>): void =>
+          setTextAreaValue(ev.target.value)
+        }
+      />
+      <label htmlFor="Whatis2+2"> What is 2 + 2 </label>
+      <select
+        id="Whatis2+2"
+        value={selectValue}
+        onBlur={(ev: React.ChangeEvent<HTMLSelectElement>): void =>
+          setSelectValue(ev.target.value)
+        }
+      >
+        <option value="4">4</option>
+        <option value="Not 4">Not 4</option>
+      </select>
     </section>
   );
 };
