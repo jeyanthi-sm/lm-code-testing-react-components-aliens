f6252d945ecb22fdc0052ad06f4f9e5e3dd25682
jeyanthi-sm <111299753+jeyanthi-sm@users.noreply.github.com>
2023-02-05 16:22:53 +0000

    Removed the cleanup function calls in Test cases and also changed the test description

--- src/SpeciesName.test.tsx
+++ src/SpeciesName.test.tsx
@@ -1,23 +1,21 @@
 /* eslint-disable testing-library/no-node-access */
-import { cleanup, render, screen } from "@testing-library/react";
-import SpeciesName, { SpeciesNameProps } from "./components/SpeciesName";
-import W12MForm from "./components/W12MForm";
+import { render, screen } from "@testing-library/react";
+import SpeciesName from "./components/SpeciesName";
 
-test("renders Species Name Label Correctly", () => {
+test("Existence of Species Name on the Screen", () => {
   const speciesName = "human";
-  const speciesNameLabelText = new RegExp(/Species Name/i);
-  cleanup();
+  const speciesNameLabelText = "Species Name";
+
   render(<SpeciesName speciesName={speciesName} />);
+
   expect(screen.getByLabelText(speciesNameLabelText)).toBeInTheDocument();
-  cleanup();
 });
 
-test("renders Species Name Value Correctly", () => {
+test("Passed Value of Species Name Displayed Correctly", () => {
   const speciesName = "human";
-  const speciesNameText = new RegExp(/human/i);
-  cleanup();
+  const speciesNameText = "human";
+
   render(<SpeciesName speciesName={speciesName} />);
 
   expect(screen.getByDisplayValue(speciesNameText)).toBeInTheDocument();
-  cleanup();
 });
