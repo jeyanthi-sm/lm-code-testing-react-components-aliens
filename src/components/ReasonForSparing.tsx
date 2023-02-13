23b37a59fefde4cc080eef0b53080b0672ca06e4
jeyanthi-sm <111299753+jeyanthi-sm@users.noreply.github.com>
2023-02-13 22:10:36 +0000

    Reason For Sparing Validation and test cases are added

--- src/components/ReasonForSparing.tsx
+++ src/components/ReasonForSparing.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
 
 export interface ReasonForSparingProps {
   value: string;
@@ -12,6 +13,18 @@ const ReasonForSparing: React.FC<ReasonForSparingProps> = (
     "We are great beings"
   );
   const { placeholder, value, onChangeHandler } = inputReasonForSparingProps;
+  const [errorMessage, setErrorMessage] = useState<string | undefined>();
+  // some validation function here
+  const validateReasonForSparing: (value: string) => string | undefined = (
+    value
+  ) => {
+    const reasonForSparingsNameRegexp = RegExp(/.{17,153}/, "mi"); //find any character not between a-z
+    const matchPattern = reasonForSparingsNameRegexp.test(value);
+    if (!matchPattern)
+      return "ERROR! Length  Must be between 17 and 153 characters Numbers and Special Characters Not allowed";
+    else return undefined;
+  };
+
   return (
     <>
       <label htmlFor="ReasonForSparingId"> Reason For Sparing </label>
@@ -19,9 +32,12 @@ const ReasonForSparing: React.FC<ReasonForSparingProps> = (
         id="ReasonForSparingId"
         value={value}
         onChange={(ev) => {
+          const errorMessage = validateReasonForSparing(ev.target.value);
+          setErrorMessage(errorMessage);
           inputReasonForSparingProps.onChangeHandler(ev.target.value);
         }}
       />
+      <ErrorMessage message={errorMessage} />
     </>
   );
 };
